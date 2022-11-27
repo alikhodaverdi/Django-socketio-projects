6a634bf9497c5287adff9b30e1602bdb292c7564
alikhodaverdi <alikhodaveerdi@yahoo.com>
2022-11-27 12:28:53 +0330

    firstCommit

---
+++ core/echo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,HttpResponse
+from django.utils.safestring import mark_safe
+import json
+
+# Create your views here.
+def index(request):
+    return render(request , 'echo/index.html')
+    
+    
